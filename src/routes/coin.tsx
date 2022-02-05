4cb4cff3546de5fe13cef2de98bea469f5fbf113
MyungWanPark <mywanpark@gmail.com>
2022-02-05 14:33:56 +0900

    use React-Router-Dom

---
+++ src/routes/coin.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Coin = () => {
+  const { coinId } = useParams();
+
+  return <h1>{coinId}</h1>;
+};
+
+export default Coin;
