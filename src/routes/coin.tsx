2983dc0e3620d9c848dd71669152601c406cb6da
MyungWanPark <mywanpark@gmail.com>
2022-02-07 00:13:32 +0900

    add nested route frame

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
+import Chart from "./chart";
+import Price from "./price";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -25,15 +27,141 @@ const Header = styled.header`
   align-items: center;
 `;
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 interface StateInterface {
   name: string;
+}
+
+interface IPriceInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: Date;
+  last_updated: Date;
+  quotes: Quotes;
+}
+
+interface Quotes {
+  USD: Usd;
+}
+
+interface Usd {
+  price: number;
+  volume_24h: number;
+  volume_24h_change_24h: number;
+  market_cap: number;
+  market_cap_change_24h: number;
+  percent_change_15m: number;
+  percent_change_30m: number;
+  percent_change_1h: number;
+  percent_change_6h: number;
+  percent_change_12h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_1y: number;
+  ath_price: number;
+  ath_date: Date;
+  percent_from_price_ath: number;
+}
+
+interface IGeneralInfo {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  tags: Tag[];
+  team: Team[];
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: Date;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: Links;
+  links_extended: LinksExtended[];
+  whitepaper: Whitepaper;
+  first_data_at: Date;
+  last_data_at: Date;
+}
+
+interface Links {
+  explorer: string[];
+  facebook: string[];
+  reddit: string[];
+  source_code: string[];
+  website: string[];
+  youtube: string[];
+}
+
+interface LinksExtended {
+  url: string;
+  type: string;
+  stats?: Stats;
+}
+
+interface Stats {
+  subscribers?: number;
+  contributors?: number;
+  stars?: number;
+  followers?: number;
+}
+
+interface Tag {
+  id: string;
+  name: string;
+  coin_counter: number;
+  ico_counter: number;
+}
+
+interface Team {
+  id: string;
+  name: string;
+  position: string;
+}
+
+interface Whitepaper {
+  link: string;
+  thumbnail: string;
 }
 
 const Coin = () => {
   const { coinId } = useParams();
   const [isLoading, setisLoading] = useState(true);
-  const [generalInfo, setGeneralInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [generalInfo, setGeneralInfo] = useState<IGeneralInfo>();
+  const [priceInfo, setPriceInfo] = useState<IPriceInfo>();
 
   const state = useLocation().state as StateInterface;
   useEffect(() => {
@@ -46,15 +174,50 @@ const Coin = () => {
       ).json();
       setGeneralInfo(generalInfo);
       setPriceInfo(priceInfo);
+      setisLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {isLoading ? <Loader>Loading...</Loader> : null}
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{generalInfo?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${generalInfo?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{generalInfo?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{generalInfo?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Routes>
+            <Route path="price" element={<Price />} />
+            <Route path="chart" element={<Chart />} />
+          </Routes>
+        </>
+      )}
     </Container>
   );
 };
