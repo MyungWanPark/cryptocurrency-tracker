5d40b2144612489e15dfdb8aec5b66ec77fb68ef
MyungWanPark <mywanpark@gmail.com>
2022-02-08 13:45:09 +0900

    get coin historical data in chart.tsx

--- src/routes/chart.tsx
+++ src/routes/chart.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
 
-const Chart = () => {
+interface ICoin {
+  coinId?: string;
+}
+
+const Chart = ({ coinId }: ICoin) => {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return <h1>chart</h1>;
 };
 
